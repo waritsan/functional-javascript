40de2700a618eb050dbb79328f3aa7e33aaf1bd8
Warit Santaputra <waritsan@gmail.com>
2018-03-05 14:03:16 +0700

    Add every-some

---
+++ every-some.js
@@ -0,0 +1,11 @@
+function checkUserValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(function (submittedUser) {
+      return goodUsers.some(function (goodUser) {
+        return goodUser.id === submittedUser.id;
+      });
+    });
+  }
+}
+
+module.exports = checkUserValid;
